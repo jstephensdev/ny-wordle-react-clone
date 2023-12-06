aafa0552027fb7ebc492c57c40f7668163bd8533
“Jessica <jessica.stephens.dev@gmail.com>
2023-12-06 09:26:50 -0600

    check for correct letters in correct place

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ This react app provides a simular experience to the New York Time's daily Wordle
 <img src="./readme-image.png" alt="NY Times Wordle React Clone" height="400" width="400" style="vertical-align:top; margin:4px">
 
 ## local start:
-npm start
+1. npm install
+2. npm start
 
 ## Features:
 1. 5 letter word and 6 guesses
@@ -12,13 +13,12 @@ npm start
 3. when a keyboard key is pressed, keys indicate yellow when the letter is in the word and gray when it is not
 4. undue letters via delete / backspace
 
-## Features Todo:
+## Things to Improve or Todo:
 1. keyboard keys remain lit with expected css style
-2. display correct key style when key is in correct place
-3. improve word selection by provide words from an api or create a larger word list
-4. make setting icon work. 
-5. add abilit to set maxCols for longer or shorter words
-6. store stats 
-7. create stats section
-8. make it so keyboard keys in view are clickable
-9. prevent backspacing keys in word row guesses pior to the current row guess
+2. improve word selection by provide words from an api or create a larger word list
+3. make setting icon work. 
+4. add ability to set maxCols for longer or shorter words
+5. store stats 
+6. create stats section
+7. make it so keyboard keys in view are clickable
+8. prevent backspacing keys in word row guesses pior to the current row guess
--- src/components/KeyGrid.jsx
+++ src/components/KeyGrid.jsx
@@ -8,28 +8,31 @@ const KeyGrid = () => {
   const [keyPressed, setKeyPressed] = useState('');
   const [showInfo, setShowInfo] = useState(false);
   const [showStats, setShowStats] = useState(false);
-  const [wordToMatch, setWordToMatch] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+  const [wordToMatch, setWordToMatch] = useState('');
   const maxRows = 6;
   const maxCols = 5;
+
+  console.log(wordToMatch)
 
   useEffect(() => {
     setWordToMatch(faker.word.adjective(maxCols));
   }, []);
 
   const handleKeyDown = (event) => {
+    setKeyPressed(event.key);
+    // set the event when key has a length of 1, the key must be lowercase a-z
     if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
-      setKeyPressed(event.key);
       setSequence((prevSequence) => [...prevSequence, event.key]);
     }
-
-    if (sequence.length % maxCols === 0 && sequence.length > 0) {
-      // Check for a match after each row is complete
-      checkForMatch();
+    // Check for a match on enter after each row is complete
+    if (event.key === 'Enter') {
+      if (sequence.length % maxCols === 0 && sequence.length > 0) {
+        checkForMatch();
+      }
     }
-
+    // Handle Backspace key to remove the previous letter
     if (event.key === 'Backspace') {
-      setKeyPressed(event.key);
-      // Handle Backspace key to remove the previous letter
       setSequence((prevSequence) => prevSequence.slice(0, -1));
     }
   };
@@ -46,12 +49,16 @@ const KeyGrid = () => {
     return sequence.length > index ? sequence[index] : '';
   };
 
-  const checkForMatch = () => {
-    // Extract the letters from the last row
-    const lastRowLetters = Array.from({ length: maxCols }, (_, colIndex) =>
+  const lastRow = () => {
+    return Array.from({ length: maxCols }, (_, colIndex) =>
       getCellValue(sequence.length - maxCols + colIndex)
     ).join('');
-    // Check if the letters in the last row match the target word
+  };
+
+  const checkForMatch = () => {
+    // Extract the letters from the last row
+    const lastRowLetters = lastRow();
+    // Check if the letters in the last row match the wordToMatch
     if (lastRowLetters === wordToMatch) {
       alert('Match! Resetting the board.');
       resetBoard();
@@ -73,10 +80,7 @@ const KeyGrid = () => {
   const infoModalContent = () => {
     return (
       <ul>
-        <li>
-          Type a five letter word and hit enter. (continuing to type the next
-          word guess works too)
-        </li>
+        <li>Type a five letter word and hit enter.</li>
         <li>
           The keyboard keys will light up as a key/letter is clicked. Yellow
           means they exist in the word and gray means the letter does not
@@ -95,6 +99,24 @@ const KeyGrid = () => {
     return <div>Stats coming soon!</div>;
   };
 
+  const settingsModalContent = () => {
+    return <div>Settings coming soon!</div>;
+  };
+
+  const inWordToMatch = () => {
+    if (keyPressed !== '') {
+      return wordToMatch.includes(keyPressed);
+    }
+  };
+
+  const isLetterMatchingAtCurrentPosition = () => {
+    let checkGrid = '';
+    for(let i = 0; i < 5; i++ ){
+      checkGrid = checkGrid.concat(wordToMatch);
+    }
+    return sequence[sequence.length - 1] === checkGrid.split('')[sequence.length - 1];
+  };
+
   return (
     <div className="key-sequence-container">
       <nav className="app-header">
@@ -102,6 +124,7 @@ const KeyGrid = () => {
           onClick={() => {
             setShowInfo(false);
             setShowStats(false);
+            setShowSettings(false);
           }}
         >
           NY Times Wordle Clone
@@ -110,7 +133,6 @@ const KeyGrid = () => {
           <ion-icon
             onClick={() => {
               setShowInfo(!showInfo);
-              setShowStats(false);
             }}
             name="help"
             size="large"
@@ -119,7 +141,6 @@ const KeyGrid = () => {
           <ion-icon
             onClick={() => {
               setShowStats(!showStats);
-              setShowInfo(false);
             }}
             size="large"
             name="stats-chart-outline"
@@ -127,6 +148,9 @@ const KeyGrid = () => {
           ></ion-icon>
           <span style={{ margin: '5px' }}>
             <ion-icon
+              onClick={() => {
+                setShowSettings(!showSettings);
+              }}
               size="large"
               name="settings"
               aria-label="Settings"
@@ -138,14 +162,35 @@ const KeyGrid = () => {
         <Modal title="How To:" content={infoModalContent()} />
       ) : showStats ? (
         <Modal title="Stats:" content={statsModalContent()} />
+      ) : showSettings ? (
+        <Modal title="Setttings:" content={settingsModalContent()} />
       ) : (
         <>
           <table className="key-sequence-table">
             <tbody>
               {Array.from({ length: maxRows }, (_, rowIndex) => (
-                <tr key={rowIndex}>
+                <tr key={rowIndex + 1}>
                   {Array.from({ length: maxCols }, (_, colIndex) => (
-                    <td key={colIndex} className="key-cell">
+                    <td
+                      key={colIndex + 1}
+                      className={
+                        keyPressed ===
+                          getCellValue(rowIndex * maxCols + colIndex) &&
+                        wordToMatch &&
+                        inWordToMatch() &&
+                        isLetterMatchingAtCurrentPosition()
+                          ? 'key-cell key-green'
+                          : keyPressed ===
+                              getCellValue(rowIndex * maxCols + colIndex) &&
+                            inWordToMatch()
+                          ? 'key-cell key-yellow'
+                          : keyPressed ===
+                              getCellValue(rowIndex * maxCols + colIndex) &&
+                            keyPressed !== ''
+                          ? 'key-cell key-gray'
+                          : 'key-cell'
+                      }
+                    >
                       {getCellValue(rowIndex * maxCols + colIndex)}
                     </td>
                   ))}
@@ -160,10 +205,13 @@ const KeyGrid = () => {
                   <div
                     className={
                       keyPressed === key.name &&
-                      wordToMatch.includes(keyPressed)
+                      wordToMatch &&
+                      inWordToMatch() &&
+                      isLetterMatchingAtCurrentPosition()
+                        ? key.class + ' key-in-correct-spot'
+                        : keyPressed === key.name && inWordToMatch()
                         ? key.class + ' key-in-word'
-                        : keyPressed === key.name &&
-                          !wordToMatch.includes(keyPressed)
+                        : keyPressed === key.name && !inWordToMatch()
                         ? key.class + ' key-not-in-word'
                         : key.class
                     }
