c683edb54599ce94a7749ff83f759de8f35d8bfd
“Jessica <jessica.stephens.dev@gmail.com>
2023-12-30 13:04:14 -0600

    reorganizing keyboard to fix mobile view

--- src/config/keyRows.js
+++ src/config/keyRows.js
@@ -1,148 +1,151 @@
 export const keyRows = [
-    [
-      {
-        id: 1,
-        key: 'q',
-        class: 'key-default',
-      },
-      {
-        id: 2,
-        key: 'w',
-        class: 'key-default',
-      },
-      {
-        id: 3,
-        key: 'e',
-        class: 'key-default',
-      },
-      {
-        id: 4,
-        key: 'r',
-        class: 'key-default',
-      },
-      {
-        id: 5,
-        key: 't',
-        class: 'key-default',
-      },
-      {
-        id: 6,
-        key: 'y',
-        class: 'key-default',
-      },
-      {
-        id: 7,
-        key: 'u',
-        class: 'key-default',
-      },
-      {
-        id: 8,
-        key: 'i',
-        class: 'key-default',
-      },
-      {
-        id: 9,
-        key: 'o',
-        class: 'key-default',
-      },
-      {
-        id: 10,
-        key: 'p',
-        class: 'key-default',
-      },
-    ],
-    [
-      {
-        id: 11,
-        key: 'a',
-        class: 'key-default',
-      },
-      {
-        id: 12,
-        key: 's',
-        class: 'key-default',
-      },
-      {
-        id: 13,
-        key: 'd',
-        class: 'key-default',
-      },
-      {
-        id: 14,
-        key: 'f',
-        class: 'key-default',
-      },
-      {
-        id: 15,
-        key: 'g',
-        class: 'key-default',
-      },
-      {
-        id: 16,
-        key: 'h',
-        class: 'key-default',
-      },
-      {
-        id: 17,
-        key: 'j',
-        class: 'key-default',
-      },
-      {
-        id: 18,
-        key: 'k',
-        class: 'key-default',
-      },
-      {
-        id: 19,
-        key: 'l',
-        class: 'key-default',
-      },
-    ],
-    [
-      {
-        id: 271,
-        key: 'Enter',
-        class: 'key-default',
-      },
-      {
-        id: 20,
-        key: 'z',
-        class: 'key-default',
-      },
-      {
-        id: 21,
-        key: 'x',
-        class: 'key-default',
-      },
-      {
-        id: 22,
-        key: 'c',
-        class: 'key-default',
-      },
-      {
-        id: 23,
-        key: 'v',
-        class: 'key-default',
-      },
-      {
-        id: 24,
-        key: 'b',
-        class: 'key-default',
-      },
-      {
-        id: 25,
-        key: 'n',
-        class: 'key-default',
-      },
-      {
-        id: 26,
-        key: 'm',
-        class: 'key-default',
-      },
-      {
-        id: 270,
-        key: 'Delete',
-        class: 'key-default',
-      },
-    ],
-  ];
+  [
+    {
+      id: 1,
+      key: 'q',
+      class: 'key-default',
+    },
+    {
+      id: 2,
+      key: 'w',
+      class: 'key-default',
+    },
+    {
+      id: 3,
+      key: 'e',
+      class: 'key-default',
+    },
+    {
+      id: 4,
+      key: 'r',
+      class: 'key-default',
+    },
+    {
+      id: 5,
+      key: 't',
+      class: 'key-default',
+    },
+    {
+      id: 6,
+      key: 'y',
+      class: 'key-default',
+    },
+    {
+      id: 7,
+      key: 'u',
+      class: 'key-default',
+    },
+    {
+      id: 8,
+      key: 'i',
+      class: 'key-default',
+    },
+  ],
+  [
+    {
+      id: 9,
+      key: 'o',
+      class: 'key-default',
+    },
+    {
+      id: 10,
+      key: 'p',
+      class: 'key-default',
+    },
+    {
+      id: 11,
+      key: 'a',
+      class: 'key-default',
+    },
+    {
+      id: 12,
+      key: 's',
+      class: 'key-default',
+    },
+    {
+      id: 13,
+      key: 'd',
+      class: 'key-default',
+    },
+    {
+      id: 14,
+      key: 'f',
+      class: 'key-default',
+    },
+    {
+      id: 15,
+      key: 'g',
+      class: 'key-default',
+    },
+    {
+      id: 16,
+      key: 'h',
+      class: 'key-default',
+    },
+  ],
+  [
+    {
+      id: 17,
+      key: 'j',
+      class: 'key-default',
+    },
+    {
+      id: 18,
+      key: 'k',
+      class: 'key-default',
+    },
+    {
+      id: 19,
+      key: 'l',
+      class: 'key-default',
+    },
+
+    {
+      id: 20,
+      key: 'z',
+      class: 'key-default',
+    },
+    {
+      id: 21,
+      key: 'x',
+      class: 'key-default',
+    },
+    {
+      id: 22,
+      key: 'c',
+      class: 'key-default',
+    },
+    {
+      id: 23,
+      key: 'v',
+      class: 'key-default',
+    },
+    {
+      id: 24,
+      key: 'b',
+      class: 'key-default',
+    },
+  ],
+  [
+    {
+      id: 25,
+      key: 'n',
+      class: 'key-default',
+    },
+    {
+      id: 26,
+      key: 'm',
+      class: 'key-default',
+    },
+    {
+      id: 271,
+      key: 'Enter',
+      class: 'key-default',
+    },
+    {
+      id: 270,
+      key: 'Delete',
+      class: 'key-default',
+    },
+  ],
+];
